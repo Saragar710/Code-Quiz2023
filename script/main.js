85467ba80eebe85149410af324f7ad717f798a95
Sara Garcia <saragca81@gmail.com>
2023-07-27 16:05:37 -0700

    trying out few options on how to go about the process

--- script/main.js
+++ script/main.js
@@ -1,41 +1,59 @@
-var myQuestions =[
-    {
-        question: "What is Stanley Hudsons favorite day?",
+var myQuestions =[ {
+//     {
+//         question: "What is Stanley Hudsons favorite day?",
 
-        answers: {
-            a: 'Bagel Fridays',
-            b: 'Taco Tuesday',
-            c: 'Pretzel Day'
-        },
+//         answers: {
+//             a: 'Bagel Fridays',
+//             b: 'Taco Tuesday',
+//             c: 'Pretzel Day'
+//         },
 
-        correctAnswer: 'c'
+//         correctAnswer: 'c'
     
-    },
-    {
-        question: "What is Phyllis's husband's name and what company does he work for?",
+//     },
+//     {
+//         question: "What is Phyllis's husband's name and what company does he work for?",
 
-        answers: {
-            a: 'Lance Bass, Bass ProShop',
-            b: 'Bob Vance, Vance Refridgeration',
-            c: 'Mike Dancer, MD Dance Studio'
-        },
+//         answers: {
+//             a: 'Lance Bass, Bass ProShop',
+//             b: 'Bob Vance, Vance Refridgeration',
+//             c: 'Mike Dancer, MD Dance Studio'
+//         },
 
-        correctAnswer: 'b'
-    },
-    {
-        question: "Where did Micheal and Jan go for Christmas Vacation?",
+//         correctAnswer: 'b'
+//     },
+//     {
+//         question: "Where did Micheal and Jan go for Christmas Vacation?",
 
-        answers: {
-            a: 'Bahamas',
-            b: 'Hawaii',
-            c: 'Jamaica'
-        },
+//         answers: {
+//             a: 'Bahamas',
+//             b: 'Hawaii',
+//             c: 'Jamaica'
+//         },
 
-        correctAnswer: 'c'
-    }
+//         correctAnswer: 'c'
+//     },
+//     {
+//         question: "Finish the sentence: Thats what...",
+
+//         answers: {
+//             a: 'she said',
+//             b: 'Im talking about',
+//             c: 'happens'
+//         }, 
+
+//         correctAnswer: 'a'
+//     }
+// ];
+    question: "What is Stanly Hudsons favorite day?",
+    answers:[
+        {text: "Bagel Friday", correct: false},
+        {text: "Taco Tuesday", correct: false},
+        {text: "Pretzel Day", correct: true},
+    ]
+}
+
 ];
-
-
 
 
 
@@ -46,10 +64,21 @@ function gernerateQuiz(questions, quizContainer, resultsContainer, submitButton)
     function showQuestions(questions, quizContainer) {
         var output=[];
         var answers;
+        showQuestions(myQuestions,quizContainer);
+        function myQuestions (){
+           getElementById[`What is Stanley Hudsons favorite day?`]
+        
+        if (i = 'c', correctAnswer = true);{
+
+        } else {
+            answers('a','b' = answers = false)        }
+
 
         for(var i=0; i<questions.length; i++){
             answers =[];
         }
+
+
     }
 
     function showResults(questions, quizContainer,resultsContainer) {
@@ -57,7 +86,7 @@ function gernerateQuiz(questions, quizContainer, resultsContainer, submitButton)
 
     }
 
-    showQuestions(questions,quizContainer);
+   
 
 
     //when user clickes submit, show results
