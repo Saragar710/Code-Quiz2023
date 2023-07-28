e692856b9a6db05be25d22d907c31fab2aa0a959
Sara Garcia <saragca81@gmail.com>
2023-07-27 18:20:15 -0700

    still trying to figure out which route to go. need to add more code so it works first

--- script/main.js
+++ script/main.js
@@ -1,59 +1,84 @@
-var myQuestions =[ {
-//     {
-//         question: "What is Stanley Hudsons favorite day?",
+var myQuestions =[ 
+    {
+        question: "What is Stanley Hudsons favorite day?",
 
-//         answers: {
-//             a: 'Bagel Fridays',
-//             b: 'Taco Tuesday',
-//             c: 'Pretzel Day'
-//         },
+        answers: {
+            a: 'Bagel Fridays',
+            b: 'Taco Tuesday',
+            c: 'Pretzel Day'
+        },
 
-//         correctAnswer: 'c'
+        correctAnswer: 'c'
     
-//     },
-//     {
-//         question: "What is Phyllis's husband's name and what company does he work for?",
+    },
+    {
+        question: "What is Phyllis's husband's name and what company does he work for?",
 
-//         answers: {
-//             a: 'Lance Bass, Bass ProShop',
-//             b: 'Bob Vance, Vance Refridgeration',
-//             c: 'Mike Dancer, MD Dance Studio'
-//         },
+        answers: {
+            a: 'Lance Bass, Bass ProShop',
+            b: 'Bob Vance, Vance Refridgeration',
+            c: 'Mike Dancer, MD Dance Studio'
+        },
 
-//         correctAnswer: 'b'
-//     },
-//     {
-//         question: "Where did Micheal and Jan go for Christmas Vacation?",
+        correctAnswer: 'b'
+    },
+    {
+        question: "Where did Micheal and Jan go for Christmas Vacation?",
 
-//         answers: {
-//             a: 'Bahamas',
-//             b: 'Hawaii',
-//             c: 'Jamaica'
-//         },
+        answers: {
+            a: 'Bahamas',
+            b: 'Hawaii',
+            c: 'Jamaica'
+        },
 
-//         correctAnswer: 'c'
-//     },
-//     {
-//         question: "Finish the sentence: Thats what...",
+        correctAnswer: 'c'
+    },
+    {
+        question: "Finish the sentence: Thats what...",
 
-//         answers: {
-//             a: 'she said',
-//             b: 'Im talking about',
-//             c: 'happens'
-//         }, 
+        answers: {
+            a: 'she said',
+            b: 'Im talking about',
+            c: 'happens'
+        }, 
 
-//         correctAnswer: 'a'
-//     }
-// ];
-    question: "What is Stanly Hudsons favorite day?",
-    answers:[
-        {text: "Bagel Friday", correct: false},
-        {text: "Taco Tuesday", correct: false},
-        {text: "Pretzel Day", correct: true},
-    ]
-}
-
+        correctAnswer: 'a'
+    }
 ];
+
+//     question: "What is Stanly Hudsons favorite day?",
+//     answers:[
+//         {text: "Bagel Friday", correct: false},
+//         {text: "Taco Tuesday", correct: false},
+//         {text: "Pretzel Day", correct: true},
+//     ]
+// },
+// {
+//     question: "What is Phyliis's husbands name and what compnay does he work for?",
+//     answer: [
+//         {text: 'Lance Bass, Bass ProShop', correct: false},
+//         {text: 'Bob Vance, Vance Refridgeration', correct: true},
+//         {text: 'Mike Dance, MD Dance Studio', correct: false},
+//     ]
+// },
+// {
+//     question: "Where did Micheal and Jan go for Christmas Vacation?",
+//     answer:[
+//         {text: 'Bahamas', correct: false},
+//         {text: 'Hawaii', correct: false},
+//         {text: 'Jamaica', correct: true},
+//     ]
+// },
+// {
+//     question: "Finish the sentence: That's what...",
+//     answer: [
+//         {text: 'she said', correct: true},
+//         {text: 'Im talking about', correct: false},
+//         {text: 'happens', correct: false},
+//     ]
+// }
+
+//];
 
 
 
@@ -71,7 +96,8 @@ function gernerateQuiz(questions, quizContainer, resultsContainer, submitButton)
         if (i = 'c', correctAnswer = true);{
 
         } else {
-            answers('a','b' = answers = false)        }
+            answers = "a, b, = false"; 
+          }
 
 
         for(var i=0; i<questions.length; i++){
@@ -94,4 +120,4 @@ function gernerateQuiz(questions, quizContainer, resultsContainer, submitButton)
 
         showResults(questions, quizContainer, resultsContainer);
     }
-}
+}}
